97913e5f01d4ccdc09b5ab3c52680a211918c2bd
joey6776 <jsavanhu@smail.pembinatrails.ca>
2025-10-29 15:31:27 -0500

    added some script with Mr Warrens help..again

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -13,3 +13,11 @@ echo ${some_variable[@]}
 echo ${some_variable[0]}
 echo ${some_variable[2]}
 echo ${some_variable[1]}
+
+open "PowerShell"
+run command-> wsl --install
+
+# this will open up a "Windows subsystem for linux" and allow you to use a linux terminal INSIDE windows. You can run and edit your bash code there :)
+
+#if you close the terminal or need to open it again Open PowerShell and run the command-> wsl -d ubuntu
+#it should open up again for you.
