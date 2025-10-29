eb45f20d6ca15ed4acffd32dc83e29f604934f4c
joey6776 <jsavanhu@smail.pembinatrails.ca>
2025-10-29 15:28:25 -0500

    added some script with Mr Warrens help

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+echo -e "
+hello Joe!
+good to meet you!
+
+"
+
+some_variable=("list" "of" "items")
+
+echo $some_variable
+echo ${some_variable[@]}
+echo ${some_variable[0]}
+echo ${some_variable[2]}
+echo ${some_variable[1]}
